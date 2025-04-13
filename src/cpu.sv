5791ae00f711d14cdb6e326bdac0f0549192ecbf
So Koide <sokoide@gmail.cojm>
2025-04-13 16:01:26 +0900

    add: Hello World! example

--- src/cpu.sv
+++ src/cpu.sv
@@ -69,9 +69,44 @@ module cpu (
 
   // program to load at startup
   // make sure to set the size boot_progarm[X]
-  logic [7:0] boot_program[17];
+  logic [7:0] boot_program[32];
 
   initial begin
+    // Hello World
+    boot_program[0]  = 8'hA0;  // LDY #0
+    boot_program[1]  = 8'h00;
+    boot_program[2]  = 8'hA2;  // LDX #0
+    boot_program[3]  = 8'h00;
+    boot_program[4]  = 8'hB9;  // loop: LDA message, Y
+    boot_program[5]  = 8'h13;
+    boot_program[6]  = 8'h02;
+    boot_program[7]  = 8'hF0;  // BEQ done
+    boot_program[8]  = 8'h07;
+    boot_program[9]  = 8'h9D;  // STA $E00, X
+    boot_program[10] = 8'h00;
+    boot_program[11] = 8'hE0;
+    boot_program[12] = 8'hC8;  // INY
+    boot_program[13] = 8'hE8;  // INX
+    boot_program[14] = 8'hD0;  // BNE loop
+    boot_program[15] = 8'hF4;
+    boot_program[16] = 8'h4C;  // done: JMP done (infinite loop)
+    boot_program[17] = 8'h10;
+    boot_program[18] = 8'h12;
+    boot_program[19] = 8'h48;  // message: Hello World!, 0
+    boot_program[20] = 8'h65;  // 'H'
+    boot_program[21] = 8'h6C;  // 'e'
+    boot_program[22] = 8'h6C;  // 'l'
+    boot_program[23] = 8'h6F;  // 'l'
+    boot_program[24] = 8'h20;  // ...
+    boot_program[25] = 8'h57;
+    boot_program[26] = 8'h6F;
+    boot_program[27] = 8'h72;
+    boot_program[28] = 8'h6C;
+    boot_program[29] = 8'h64;
+    boot_program[30] = 8'h21;
+    boot_program[31] = 8'h00;  // 0 terminator
+
+
     // // JSR
     // boot_program[0]  = 8'hBA;  // TSX
     // boot_program[1]  = 8'h20;  // JSR foo ($0209)
@@ -101,25 +136,25 @@ module cpu (
     // boot_program[11] = 8'h00;
     // boot_program[12] = 8'h03;
 
-    // Simple 3)
-    // copy 0x00-0x7F to 0xE000-0xE07F (VRAM)
-    boot_program[0]  = 8'hA0;  // LDY #$00
-    boot_program[1]  = 8'h00;
-    boot_program[2]  = 8'hA2;  // LDX #$00
-    boot_program[3]  = 8'h00;
-    boot_program[4]  = 8'h8A;  // TXA (A=X)
-    boot_program[5]  = 8'h99;  // STA $E000, Y
-    boot_program[6]  = 8'h00;
-    boot_program[7]  = 8'hE0;
-    boot_program[8]  = 8'hE8;  // INX
-    boot_program[9]  = 8'hC8;  // INY
-    boot_program[10] = 8'hC0;  // CPY #$7F
-    boot_program[11] = 8'h7F;  //
-    boot_program[12] = 8'hD0;  // BNE -10;  PC+2-10=4 (F6=-10)
-    boot_program[13] = 8'hF6;
-    boot_program[14] = 8'h18;  // CLC
-    boot_program[15] = 8'h90;  // BCC -2; PC+2-2=here
-    boot_program[16] = 8'hFE;  //
+    // // Simple 3)
+    // // copy 0x00-0x7F to 0xE000-0xE07F (VRAM)
+    // boot_program[0]  = 8'hA0;  // LDY #$00
+    // boot_program[1]  = 8'h00;
+    // boot_program[2]  = 8'hA2;  // LDX #$00
+    // boot_program[3]  = 8'h00;
+    // boot_program[4]  = 8'h8A;  // TXA (A=X)
+    // boot_program[5]  = 8'h99;  // STA $E000, Y
+    // boot_program[6]  = 8'h00;
+    // boot_program[7]  = 8'hE0;
+    // boot_program[8]  = 8'hE8;  // INX
+    // boot_program[9]  = 8'hC8;  // INY
+    // boot_program[10] = 8'hC0;  // CPY #$7F
+    // boot_program[11] = 8'h7F;  //
+    // boot_program[12] = 8'hD0;  // BNE -10;  PC+2-10=4 (F6=-10)
+    // boot_program[13] = 8'hF6;
+    // boot_program[14] = 8'h18;  // CLC
+    // boot_program[15] = 8'h90;  // BCC -2; PC+2-2=here
+    // boot_program[16] = 8'hFE;  //
 
     // Simple 2) draw 'B' on top-left using zero page
     // // address 00 <- 0x00
