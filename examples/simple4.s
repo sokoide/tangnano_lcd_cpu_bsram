099727319a4b64d57a5e59e7fd0c1ca3607d24ed
So Koide <sokoide@gmail.cojm>
2025-04-19 14:18:59 +0900

    change sleep interval

--- examples/simple4.s
+++ examples/simple4.s
@@ -10,12 +10,12 @@ loop:
     STA $01
 ; display 'A'
     STA $E000
-; WVS: wait for 0.5 second
-    .byte $FF, $1D
+; WVS: wait for 1 second
+    .byte $FF, $3A
 ; IFO: show registers and memory at $0000-$007F
     .byte $DF,$00,$00
-; WVS: wait for 0.5 second
-    .byte $FF, $1D
+; WVS: wait for 1 second
+    .byte $FF, $3A
 ; CLV: clear VRAM
     .byte $CF
 ; inclement A register
